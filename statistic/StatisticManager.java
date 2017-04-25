12b3a471ff0362635ffa6bdbb153b4ffd6532ada
HasanKarilov <hkarilov@gmail.com>
2017-04-25 15:11:58 +0600

    StatisticStorage inner class 13

--- statistic/StatisticManager.java
+++ statistic/StatisticManager.java
@@ -1,11 +1,28 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /** С его помощью будем регистрировать события в хранилище
  * У нас должно быть одно хранилище с одной точкой входа
  */
 public class StatisticManager {
+    private class StatisticStorage{
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+        private StatisticStorage(){
+            for(EventType eventType: EventType.values()){
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
+    }
+
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
     private StatisticManager(){
     }
     private static StatisticManager instance = new StatisticManager();
@@ -17,4 +34,5 @@ public class StatisticManager {
     public void register(EventDataRow data){
 
     }
+
 }
